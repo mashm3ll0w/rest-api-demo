d5e85483623b76cc4c3f25d99db7d06788b7e316
Charles Swaleh <charlswall@live.com>
2023-06-24 17:34:21 +0300

    View a single user order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,4 +6,16 @@ class OrdersController < ApplicationController
     render json: orders, status: :ok
   end
 
+  def show
+    current_user = current_user()
+    orders = current_user.orders
+    single_order = orders.select {|order| order.id == params[:id].to_i}
+
+    if single_order.length > 0
+      render json: single_order, status: :ok
+    else
+      render json: {error: "You don't have such an order"}
+    end
+  end
+
 end
