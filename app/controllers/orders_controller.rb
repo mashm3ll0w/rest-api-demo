cf8191b19532c79fd3a47cce40ce79d93b2b8f28
Charles Swaleh <charlswall@live.com>
2023-06-24 17:29:57 +0300

    View all of a users orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,9 @@
 class OrdersController < ApplicationController
+
+  def index
+    current_user = current_user()
+    orders = current_user.orders
+    render json: orders, status: :ok
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :menus, only: [:show, :index]
   end
   resources :restaurants, only: [:index]
+  resources :orders, only: [:index, :show, :update]
 
   post "/restaurants/:restaurant_id/menus/:id", to: "menus#create"
   post "/signup", to: "users#create"
