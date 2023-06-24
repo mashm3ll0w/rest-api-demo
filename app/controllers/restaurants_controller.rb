2da6c791275360b2481d909a5d24be58b6962d40
Charles Swaleh <charlswall@live.com>
2023-06-24 12:41:26 +0300

    Added the routing for the default get route /

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,8 @@
 class RestaurantsController < ApplicationController
+  skip_before_action :authorized, only: :index
+
+  def index
+    restaurants = Restaurant.all
+    render json: restaurants
+  end
 end
