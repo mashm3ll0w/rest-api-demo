19f3b7921fbe8ce19b6fc2ee571d048383b1acb4
Charles Swaleh <charlswall@live.com>
2023-06-24 12:30:56 +0300

    Created the helper method to handle authorization and authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,39 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+  before_action :authorized
+
+  def encode_token(payload)
+    JWT::encode(payload, "dGgxNTE1dGgzbTA1dHMzY3VyM3BhNTV3MHJkM3YzUg")
+  end
+
+  def auth_header
+    request.headers["Authorization"]
+  end
+
+  def decode_token
+    jwt = nil
+     if auth_header
+       token = auth_header.split(' ')[1]
+      begin
+       jwt = JWT::decode(token, "dGgxNTE1dGgzbTA1dHMzY3VyM3BhNTV3MHJkM3YzUg", true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+     end
+  end
+
+  def current_user
+    if decode_token
+      user_id = decode_token[0]['user_id']
+      user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { message: 'Login/SignUp to continue' }, status: :unauthorized unless logged_in?
+  end
 end
